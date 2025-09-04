ef9d60f3e086290fda446cab58503bc437709273
Ariel Mazara <arielmazara@gmail.com>
2025-09-03 21:04:09 -0300

    Se hace el ejercicio 6 Java con JOptionPane Tarea clase 03-09-2025

--- Java/Clase 4 Clases y Objetos Parte 2 Tarea/Ejercicio6JOption/Ejercicio6Joption/src/ejercicio6joption/Ejercicio6Joption.java
+++ Java/Clase 4 Clases y Objetos Parte 2 Tarea/Ejercicio6JOption/Ejercicio6Joption/src/ejercicio6joption/Ejercicio6Joption.java
@@ -1,20 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package ejercicio6joption;
+
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author Ariel
+ * @author ArielMazara
  */
 public class Ejercicio6Joption {
+public static void main(String[] args) {
+        int numero;
+        int suma = 0;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        do {
+            // Pedir el número usando una ventana de diálogo
+            String entrada = JOptionPane.showInputDialog("Digite un número (0 para terminar):");
+            
+            // Convertimos el texto (String) ingresado a un número entero (int)
+            numero = Integer.parseInt(entrada);
+            
+            // Acumur el valor en la variable suma
+            suma += numero;
+
+        } while (numero != 0); // El ciclo continúara mientras el número sea diferente de 0
+
+        // Al salir del bucle, muestra la suma total en una ventana de mensaje
+        JOptionPane.showMessageDialog(null, "La suma de todos los números introducidos es: " + suma);
     }
-    
 }
