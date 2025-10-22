47462e8a536738078f574f0be7dcdedd986638c9
Damián Adolfo Ponce de León <poncedeleondamianadolfo@gmail.com>
2025-10-22 15:46:17 -0300

    15.2 Creamos la Orden Parte3

--- Python/Clase 11 POO Parte 8 Diseño de clases y Sobre Carga de Operadores/Orden.py
+++ Python/Clase 11 POO Parte 8 Diseño de clases y Sobre Carga de Operadores/Orden.py
@@ -14,7 +14,8 @@ class orden:
         self._productos.append(producto) # esto es para agregar el nuevo producto
 
 
-#15.3 Creamos la clase Orden: Parte 2
+#15.2 Creamos la clase Orden: Parte 2
+
 def calcular_total(self):
     total = 0 #variable temporal para almacenar el total temporal
     for producto in self._productos:
@@ -28,6 +29,10 @@ def __str__(self):
     return f"Orden: (self.id_orden), \nProducto: (producto_str)"
 
 
+class Orden:
+    pass
+
+
 if __name__ == "__main__":
     producto1 = Producto("Camiseta", 100.00)
     print(producto1)
@@ -36,5 +41,16 @@ if __name__ == "__main__":
     orden1 = Orden(producto1) #Primer objeto orden pasando la lista de productos
     print(orden1)
 
+##15.2 Creamos la clase Orden: Parte 3
+    //orden2 = Orden(producto1)
+    //print(orden2)
 
+#Tarea: Modificar la ordem2, ingresando nuevos productos cn sus nombrers y precios
+#crear una nueva lista de productos y agregarla a la orden2
 
+    producto1 = Producto("Remera", 50.00)
+    print(producto1)
+    producto2 = Producto("Sombrero", 200.00)
+    producto1 =[producto1, producto2] #Lista de Priductos
+    orden2 = Orden(producto1) #Primer objeto orden pasando la lista de productos
+    print(orden2)
